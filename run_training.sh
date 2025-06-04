38d4671aa6f962af119185219b8d1fc6a36e22f8
tobias-fllnr <tobiasfellner99@gmail.com>
2025-06-04 10:16:43 +0200

    initial commit

--- run_training.sh
+++ run_training.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python3 ./utils/submit_multiple_jobs.py -slurm "no"
+module load spack/default
+
+module load gcc/12.3.0
+
+module load python/3.12.1
+
+# Activate the virtual environment
+source /home/tfellner/.venv/bin/activate
+
+python3 ./utils/submit_multiple_jobs.py -slurm "yes"
